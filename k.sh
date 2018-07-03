afc572b2a637160385371c45c603d647547c7688
Jeff Cox <jcox@paloaltonetworks.com>
2018-07-03 12:10:59 -0700

    good grief.  back to manual patch + fix typo

--- k.sh
+++ k.sh
@@ -9,7 +9,7 @@ k () {
   # Stop stat failing when a directory contains either no files or no hidden files
   # Track if we _accidentally_ create a new global variable
   setopt local_options null_glob typeset_silent no_auto_pushd nomarkdirs pushd_silent
-  unetopt pushd_ignore_dups
+  unsetopt pushd_ignore_dups
 
   # Process options and get files/directories
   typeset -a o_all o_almost_all o_human o_si o_directory o_no_directory o_no_vcs o_help
